ca20bb68b67b451a6e843446cf98d835ac375d7f
Luke Sun <luke@soft.pw>
2026-06-04 20:14:14 +0700

    Prepare public web for Cloudflare Pages

--- apps/public-web/public-env.example.js
+++ apps/public-web/public-env.example.js
@@ -1,4 +1,4 @@
 window.__TICKBASE_NEWS_CONFIG__ = {
-  PUBLIC_API_BASE_URL: "https://api.news.thetickbase.com",
+  PUBLIC_API_BASE_URL: "https://news-api.thetickbase.com",
   PUBLIC_WEB_DEMO: false
 };
